5a1a19e608436774d554ce1d8177e62702b1db96
Gustavus Shaw II <gshaw1997@gmail.com>
2018-01-04 15:41:09 -0500

    Added untracked files

---
+++ lib/index.ts
@@ -0,0 +1,10 @@
+import { lengths, levels, verbs, assessments, quizzes, instructions } from './taxonomy';
+
+export {
+    lengths,
+    levels,
+    verbs,
+    assessments,
+    quizzes,
+    instructions
+};
