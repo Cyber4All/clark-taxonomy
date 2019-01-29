071b0b0ba35c44541cf0267f96ceed9e3a62f10f
Gustavus Shaw II <gshaw1997@gmail.com>
2019-01-29 14:21:48 -0500

    :chore: Remove whitespace

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,7 @@
 import { taxonomy } from './taxonomy';
-
 const lengths = taxonomy.lengths;
 const levels = Object.keys(taxonomy.taxons).map(key => key.toLowerCase());
 export { taxonomy, lengths, levels };
+
+
+
